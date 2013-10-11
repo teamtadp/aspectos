a86c5c4ae5b46450f3dcb0039945e848aef032ae
FranciscoPablo <fpdecuzzi@gmail.com>
2013-10-10 22:12:08 -0300

    - Metodos en observer para quitar uno o todos los aspects asociados a el.

--- test/observer_test_spec.rb
+++ test/observer_test_spec.rb
@@ -21,4 +21,17 @@ describe 'Test de observer' do
     expect(MethodObserver.get_instance.aspects.count == 3)
   end
 
+  it 'pasa si la se pudieron quitar 2 aspects del singleton' do
+    MethodObserver.get_instance.remove_aspect @aspect2
+    MethodObserver.get_instance.remove_aspect @aspect3
+    expect(MethodObserver.get_instance.aspects.count == 1)
+  end
+
+  it 'pasa si la se pudieron quitar todos' do
+    MethodObserver.get_instance.add_aspect @aspect1
+    MethodObserver.get_instance.add_aspect @aspect2
+    MethodObserver.get_instance.remove_all
+    expect(MethodObserver.get_instance.aspects.count == 0)
+  end
+
 end
