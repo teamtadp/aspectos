79dd431bc57de89ffe86ad2213682b2f8fda78e8
Aldana Laura Quintana Munilla <qm_al@hotmail.com>
2013-10-24 14:25:28 -0300

    fix porq lo que estaba tiraba error a veces

--- src/join_point/join_point_regex_method.rb
+++ src/join_point/join_point_regex_method.rb
@@ -8,6 +8,7 @@ class JoinPointRegexMethod < AbstractJoinPoint
     #Si preguntas por a_class.instance_method(a_method).to_s.match @a_regex_pattern o  a_regex_pattern.match a_class.instance_method(a_method).to_s
     #no funciona, hay que ver pq es, pero de esta forma anda joya.
     # NO SE ASUSTEN PQ MARCA EL .match EN ROJO!!, funciona igual
-    a_class.instance_method(a_method.name) != nil && (@jp_a_regex_pattern.match a_method.name) != nil
+    (a_class.instance_methods.include?(a_method.name) || a_class.methods.include?(a_method.name))&&
+        (@jp_a_regex_pattern.match a_method.name) != nil
   end
 end
