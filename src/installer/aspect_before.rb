fb8e84dbe79a6ff25c3b4ae732d0739c9f9935bf
Aldana Laura Quintana Munilla <qm_al@hotmail.com>
2013-10-24 15:31:25 -0300

    fix de aspect_before

--- src/installer/aspect_before.rb
+++ src/installer/aspect_before.rb
@@ -7,10 +7,9 @@ class AspectBefore < AbstractAspect
       a_class.send(:define_method, 'm_with_aspect') do #TODO: poner nombre dinmico para m_with_aspect
         aspect.before_method          #TODO: ver como hacer para q el usuario pueda definir parametros (before_method(*params), para q le lleguen))
         self.send(:m_without_aspect)   #TODO: poner nombre dinamico para m_without_aspect (idea, se puede reemplazar la palabra "aspect")
-
-        a_class.send(:alias_method, :m_without_aspect, a_method)
-        a_class.send(:alias_method, a_method,'m_with_aspect'.to_sym)
       end
+      a_class.send(:alias_method, :m_without_aspect, a_method)
+      a_class.send(:alias_method, a_method,'m_with_aspect'.to_sym)
     end
 
     def before_method; raise 'before_method must be implemented' end     #esto es lo q tiene q definir el usuario
