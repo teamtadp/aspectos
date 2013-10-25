fd138903ebbffdcdc946b51bdeed29a3e8ef07b2
Aldana Laura Quintana Munilla <qm_al@hotmail.com>
2013-10-25 15:44:42 -0300

    fix para que llame cuando los parametros son cero

--- src/installer/aspect_before.rb
+++ src/installer/aspect_before.rb
@@ -4,15 +4,13 @@ class AspectBefore < AbstractAspect
   #TODO: se pueden extraer cachos de código en la super clase porq algunas cosas se van a repetir. Template method.
     def apply_aspect_method(a_method,a_class)
       aspect = self
-      method_with_aspect = a_method.to_s + '_aspect_' + self.class.name
-      method_without_aspect = a_method.to_s + '_without_aspect_' + self.class.name
+      method_with_aspect = a_method.to_s + '_aspect_' #+ self.class.name
+      method_without_aspect = a_method.to_s + '_without_aspect_' #+ self.class.name
       parameters = a_method.parameters
 
       a_class.send(:define_method, method_with_aspect)  do |*parameters|
          aspect.before_method        #TODO: ver como hacer para q el usuario pueda definir parametros (el metodo del before)(before_method(*params), para q le lleguen))
-          if parameters.size > 0
             self.send(method_without_aspect.to_sym,*parameters)   #TODO hacer q esto pueda recibir parametros (el metodo original)
-          end
        end
 
       a_class.send(:alias_method, method_without_aspect.to_sym, a_method.name)
