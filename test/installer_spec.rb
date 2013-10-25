ac7e0637b44c893c31d021a5c9bd5f48516e54e3
Aldana Laura Quintana Munilla <qm_al@hotmail.com>
2013-10-25 15:28:45 -0300

    saque codigo repetido y quedo todo funcionando con 0 1 o mas parametros

--- test/installer_spec.rb
+++ test/installer_spec.rb
@@ -8,39 +8,62 @@ require_relative '../src/cut_point/cut_point_and'
 describe 'Funciona se se instala el aspect beforer' do
 
   before(:all) do
-    class ClassApected
-      def method1 num
-        puts 'Se ejecuto el metodo m'
+    class ClassAspected
+      def method0
+      puts 'Se ejecuto el metodo method0'
+      end
+
+      def method1(num)
+        puts 'Se ejecuto el metodo mehod1'
+        puts num.to_s
+      end
+
+      def method2(num1, num2)
+        puts 'Se ejecuto el metodo method2'
+        puts num1.to_s + num2.to_s
       end
     end
+
+    class Aspecto < AspectBefore
+      attr_accessor :counter
+
+      def before_method
+        @counter =  1
+        puts 'Se ejecuto el before.'
+      end
+
+    end
+
+    join_point = JoinPointClass.new(ClassAspected)
+    join_points = Array.new
+    join_points << join_point
+    @aspect = Aspecto.new(CutPointAnd.new(join_points))
+    aspects = Array.new
+    aspects << @aspect
+    classes = Array.new
+    classes << ClassAspected
+    installer = AspectInstaller.new
+    installer.install_clases_with_aspects classes,aspects
+
+
   end
 
-  it 'should do something' do
-  class Aspecto < AspectBefore
-     attr_accessor :counter
-
-     def before_method
-       @counter =  1
-       puts 'Se ejecuto el before.'
-     end
-
+  it 'should do something 0' do
+    prueba = ClassAspected.new
+    prueba.method0
+    expect(@aspect.counter).to eq(1)
   end
-  join_point = JoinPointClass.new(ClassApected)
-  join_points = Array.new
-  join_points << join_point
-  aspect = Aspecto.new(CutPointAnd.new(join_points))
-  aspects = Array.new
-  aspects << aspect
-  classes = Array.new
-  classes << ClassApected
-  installer = AspectInstaller.new
-  installer.install_clases_with_aspects classes,aspects
 
-  prueba = ClassApected.new
+  it 'should do something 1' do
+    prueba = ClassAspected.new
+    prueba.method1 1
+    expect(@aspect.counter).to eq(1)
+  end
 
-  prueba.method1 1
-
-  expect(aspect.counter).to eq(1)
+  it 'should do something 2' do
+    prueba = ClassAspected.new
+    prueba.method2 1,2
+    expect(@aspect.counter).to eq(1)
   end
 
 end
