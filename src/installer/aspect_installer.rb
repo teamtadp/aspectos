c84a1b28abe68cb1a5893c41bbf32f0099d533f9
Aldana Laura Quintana Munilla <qm_al@hotmail.com>
2013-10-25 00:52:49 -0300

    fix del aspect before y del tests del installer, manganeta para q por ahora se redefinan solo los metodos q provee la clase

--- src/installer/aspect_installer.rb
+++ src/installer/aspect_installer.rb
@@ -3,7 +3,7 @@ require_relative 'abstract_aspect'
 class AspectInstaller
   def install_clases_with_aspects(some_clases,some_aspects)
     some_clases.each do |c|
-      c.instance_methods.each do |m| #ver esto, para mi habría que agregar c.methods también
+      c.instance_methods(false).each do |m| #ver esto, para mi habría que agregar c.methods también
         some_aspects.each do |a|
           a.apply(c.instance_method(m),c)
         end
--- test/installer_spec.rb
+++ test/installer_spec.rb
@@ -1,46 +1,46 @@
 require 'rspec'
-require_relative "../src/installer/aspect_before"
-require_relative "../src/installer/aspect_installer"
-require_relative "../src/join_point/join_point_regex_method"
-require_relative "../src/cut_point/cut_point_and"
+require_relative '../src/installer/aspect_before'
+require_relative '../src/installer/aspect_installer'
+require_relative '../src/join_point/join_point_class'
+require_relative '../src/cut_point/cut_point_and'
 
 
 describe 'Funciona se se instala el aspect beforer' do
 
   before(:all) do
     class ClassApected
-      def m
-        puts 'Se ejecutó el método m'
+      def method1
+        puts 'Se ejecuto el metodo m'
       end
     end
   end
 
   it 'should do something' do
-    class Aspecto < AspectBefore
-      attr_accessor :counter
+  class Aspecto < AspectBefore
+     attr_accessor :counter
 
-      def before_method
-        @counter =  1
-        puts "Se ejecutó el before."
-      end
-
+     def before_method
+       @counter =  1
+       puts 'Se ejecuto el before.'
      end
-    joinPoint = JoinPointRegexMethod.new /^([a-zA-Z_]+|\[\])[\?!=]?$/
-    joinPoints = Array.new
-    joinPoints << joinPoint
-    aspect = Aspecto.new(CutPointAnd.new(joinPoints))
-    aspects = Array.new
-    aspects << aspect
-    classes = Array.new
-    classes << ClassApected
-    installer = AspectInstaller.new
-    installer.install_clases_with_aspects classes,aspects
 
-    prueba = ClassApected.new
+  end
+  join_point = JoinPointClass.new(ClassApected)
+  join_points = Array.new
+  join_points << join_point
+  aspect = Aspecto.new(CutPointAnd.new(join_points))
+  aspects = Array.new
+  aspects << aspect
+  classes = Array.new
+  classes << ClassApected
+  installer = AspectInstaller.new
+  installer.install_clases_with_aspects classes,aspects
 
-    prueba.m
+  prueba = ClassApected.new
 
-    expect(aspect.counter).to eq(1)
-   end
+  prueba.method1
+
+  expect(aspect.counter).to eq(1)
+  end
 
 end
