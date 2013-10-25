ea03f5e5b1a1f0d3424f7ab827ddc75fcaa8f7c1
Aldana Laura Quintana Munilla <qm_al@hotmail.com>
2013-10-25 18:47:57 -0300

    'tests' no como la gente

--- src/installer/abstract_aspect_on_error.rb
+++ src/installer/abstract_aspect_on_error.rb
@@ -1,10 +1,11 @@
 require_relative 'abstract_aspect'
+
 class AbstractAspectOnError < AbstractAspect
   def apply_concrete_aspect_method(a_class, aspect, m_with_aspect, m_without_aspect)
     a_class.send(:define_method, m_with_aspect) do |*parameters|
       begin
       self.send(m_without_aspect.to_sym, *parameters)
-      rescue Error
+      rescue
       aspect.on_error_method(*parameters)
       end
     end
