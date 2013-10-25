9e63e2cad8524b45c4f1d26302af996d90e093e2
FranciscoPablo <fpdecuzzi@gmail.com>
2013-10-25 01:11:57 -0300

    - Se agrega el manejo de parámetros para la redefinición de métodos

--- src/installer/aspect_before.rb
+++ src/installer/aspect_before.rb
@@ -6,10 +6,19 @@ class AspectBefore < AbstractAspect
       aspect = self
       method_with_aspect = a_method.to_s + '_aspected'              #TODO:hacer q estos nombres se hagan dinamicos con el nombre del aspecto
       method_without_aspect = a_method.to_s + '_without_aspect'
-      a_class.send(:define_method, method_with_aspect)  do
-        aspect.before_method        #TODO: ver como hacer para q el usuario pueda definir parametros (el metodo del before)(before_method(*params), para q le lleguen))
-        self.send(method_without_aspect.to_sym)      #TODO hacer q esto pueda recibir parametros (el metodo original)
+      parameters = a_method.parameters
+      if (parameters.size > 0)
+        a_class.send(:define_method, method_with_aspect)  do |parameters|
+          aspect.before_method        #TODO: ver como hacer para q el usuario pueda definir parametros (el metodo del before)(before_method(*params), para q le lleguen))
+          self.send(method_without_aspect.to_sym,parameters)      #TODO hacer q esto pueda recibir parametros (el metodo original)
+        end
+      else
+        a_class.send(:define_method, method_with_aspect)  do
+          aspect.before_method        #TODO: ver como hacer para q el usuario pueda definir parametros (el metodo del before)(before_method(*params), para q le lleguen))
+          self.send(method_without_aspect.to_sym)      #TODO hacer q esto pueda recibir parametros (el metodo original)
+        end
       end
+
       a_class.send(:alias_method, method_without_aspect.to_sym, a_method.name)
       a_class.send(:alias_method, a_method.name,method_with_aspect.to_sym)
     end
